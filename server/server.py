93000271e692280984e947879370b220838b6b19
Dhruvraj Singh <64192503+dhruvraj508@users.noreply.github.com>
2024-01-05 18:14:50 -0700

    webpage in progress

--- server/server.py
+++ server/server.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
+import util
 
 app = Flask(__name__)
 
 @app.route('/classify', methods=['POST', 'GET'])
 def classify():
-    pass
+    image_data = request.form['image_data']
+    response = jsonify(util.classify_image(image_data))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
+    print('Starting Python Flask Server For World Leaders Image Classification...')
+    util.load_artifacts()
     app.run(port=5000, debug=True)
